99c8ea6ac5810ecf1633bf816474408f8924b1cf
The Son Dev <java.sonpro01@gmail.com>
2023-08-28 09:27:35 +0700

    Side Bar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,21 @@
+import SideBarNav from "./Side-Bar"
+import "./Admin.scss"
+import { FaBars } from "react-icons/fa"
+import { useState } from "react"
 const Admin = (props) => {
+
+    const [collapsed, setCollapsed] = useState(false)
     return (
-        <div>Admin componet</div>
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBarNav collapsed={collapsed}></SideBarNav>
+            </div>
+
+            <div className="admin-content">
+                Conten go here
+                <FaBars onClick={() => setCollapsed(!collapsed)} />
+            </div>
+        </div>
     )
 }
 export default Admin
---
+++ src/components/Admin/Side-Bar.js
@@ -0,0 +1,93 @@
+import 'react-pro-sidebar/dist/css/styles.css';
+import {
+    ProSidebar,
+    Menu,
+    MenuItem,
+    SubMenu,
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
+} from 'react-pro-sidebar';
+import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
+import sidebarBg from '../../assets/bg2.jpg';
+import { DiReact } from 'react-icons/di'
+import { MdDashboard } from 'react-icons/md'
+import './SideBar.scss'
+
+const SideBarNav = (props) => {
+    const { image, collapsed, toggled, handleToggleSidebar } = props
+    return (
+        <>
+            <ProSidebar
+                image={sidebarBg}
+                collapsed={collapsed}
+                toggled={toggled}
+                breakPoint="md"
+                onToggle={handleToggleSidebar}
+            >
+                <SidebarHeader>
+                    <div
+                        style={{
+                            padding: '24px',
+                            textTransform: 'uppercase',
+                            fontWeight: 'bold',
+                            fontSize: 14,
+                            letterSpacing: '1px',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        <DiReact size={'3em'} color={'00bfff'}></DiReact>
+                        <span>The Son Channel</span>
+                    </div>
+                </SidebarHeader>
+
+                <SidebarContent>
+                    <Menu iconShape="circle">
+                        <MenuItem
+                            icon={<MdDashboard />}
+                        >
+                            Dashboard
+                        </MenuItem>
+                        <MenuItem icon={<FaGem />}> Components </MenuItem>
+                    </Menu>
+                    <Menu iconShape="circle">
+                        <SubMenu
+                            icon={<FaGem />}
+                            title="Features"
+                        >
+                            <MenuItem>Quản lí Users</MenuItem>
+                            <MenuItem>Quản lí bài thi</MenuItem>
+                            <MenuItem>Quản lí câu hỏi</MenuItem>
+                        </SubMenu>
+
+                    </Menu>
+                </SidebarContent>
+
+                <SidebarFooter style={{ textAlign: 'center' }}>
+                    <div
+                        className="sidebar-btn-wrapper"
+                        style={{
+                            padding: '20px 24px',
+                        }}
+                    >
+                        <a
+                            href="https://github.com/azouaoui-med/react-pro-sidebar"
+                            target="_blank"
+                            className="sidebar-btn"
+                            rel="noopener noreferrer"
+                        >
+                            <FaGithub />
+                            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                                Thế Sơn IT
+                            </span>
+                        </a>
+                    </div>
+                </SidebarFooter>
+            </ProSidebar>
+        </>
+    )
+}
+
+export default SideBarNav
